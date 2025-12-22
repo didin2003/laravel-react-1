9d048600d81d3357ea0bd069300c53c9e945774a
didin <didinpg8080@@gmail.com>
2025-12-22 15:13:34 +0530

    deleted file

--- laravel-backend/config/cors.php
+++ laravel-backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://sonu.didin.in'],
+    'allowed_origins' => ['https://frontend-domain-name'],
 
     'allowed_origins_patterns' => [],
 
