ad70f1e600e33ce5cb42eeba31739cc91e58941b
didin2003 <didinpg8080@gmail.com>
2026-05-14 14:55:48 +0530

    Allow all origins in CORS configuration

--- laravel-backend/config/cors.php
+++ laravel-backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://frontend-domain-name'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
